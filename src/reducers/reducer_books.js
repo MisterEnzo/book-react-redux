a1538550a4c07ac65bab2c209f4e4fd1dc07dcae
Enzo Naguiat <enzo_naguiat@yahoo.com>
2018-06-18 12:58:02 +0800

    completed app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: "Harry Potter", pages: 300 },
+    { title: "Eloquent Ruby", pages: 10 },
+    { title: "Dark Tower", pages: 350 },
+    { title: "Lord of the Rings", pages: 500 }
+  ]
+};
